6885d256d9892ff68166a399ab297a781477d35c
jess <D00169978@student.dkit.ie>
2018-03-12 17:01:55 +0000

    wayfinding map

---
+++ app/src/main/java/com/example/chongjiale/navr/WayFindingActivity.java
@@ -0,0 +1,164 @@
+package com.example.chongjiale.navr;
+
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.PointF;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.indooratlas.android.sdk.IALocation;
+import com.indooratlas.android.sdk.IALocationListener;
+import com.indooratlas.android.sdk.IALocationManager;
+import com.indooratlas.android.sdk.IARegion;
+import com.indooratlas.android.sdk.resources.IAFloorPlan;
+import com.indooratlas.android.sdk.resources.IALatLng;
+import com.indooratlas.android.sdk.resources.IALocationListenerSupport;
+import com.indooratlas.android.sdk.resources.IAResourceManager;
+import com.indooratlas.android.sdk.resources.IAResult;
+import com.indooratlas.android.sdk.resources.IAResultCallback;
+import com.indooratlas.android.sdk.resources.IATask;
+
+import com.indooratlas.android.wayfinding.IARoutingLeg;
+import com.indooratlas.android.wayfinding.IAWayfinder;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
+import com.squareup.picasso.Target;
+
+import java.io.InputStream;
+
+public class WayFindingActivity extends AppCompatActivity {
+
+    public static final String TAG = "WayFinder Log";
+    private static final int MY_PERMISSION_ACCESS_FINE_LOCATION = 42;
+    /* used to decide when bitmap should be downscaled */
+    private static final int MAX_DIMENSION = 2048;
+
+    private IALocationManager mIALocationManager;
+    private IAResourceManager mResourceManager;
+    private IATask<IAFloorPlan> mPendingAsyncResult;
+    private IAFloorPlan mFloorPlan;
+    private BlueDotView mImageView;
+    private IARegion mOverlayFloorPlan = null;
+    private IATask<IAFloorPlan> mFetchFloorPlanTask;
+    private Target mLoadTarget;
+    private IAWayfinder mWayfinder;
+
+    private IARoutingLeg[] mCurrentRoute;
+
+    private Integer mFloor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_way_finding);
+
+        String graphJSON = loadGraphJSON();
+        if (graphJSON == null) {
+            Toast.makeText(this, "Could not find wayfinding_graph.json from raw " +
+                    "resources folder. Cannot do wayfinding.", Toast.LENGTH_LONG).show();
+        } else {
+            mWayfinder = IAWayfinder.create(this, graphJSON);
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startListeningPlatformLocations();
+                }
+                break;
+            }
+        }
+    }
+
+    private IALocationListener mLocationListener = new IALocationListenerSupport() {
+        @Override
+        public void onLocationChanged(IALocation location) {
+            Log.d(TAG, "location is: " + location.getLatitude() + "," + location.getLongitude());
+            String latlong="location is: " + location.getLatitude() + "," + location.getLongitude();
+            Toast.makeText(WayFindingActivity.this, latlong, Toast.LENGTH_SHORT).show();
+            if (mImageView != null && mImageView.isReady()) {
+                IALatLng latLng = new IALatLng(location.getLatitude(), location.getLongitude());
+                PointF point = mFloorPlan.coordinateToPoint(latLng);
+                mImageView.setDotCenter(point);
+                mImageView.postInvalidate();
+            }
+        }
+    };
+
+    private IARegion.Listener mRegionListener = new IARegion.Listener() {
+
+        @Override
+        public void onEnterRegion(IARegion region) {
+            if (region.getType() == IARegion.TYPE_FLOOR_PLAN) {
+                String id = region.getId();
+                //Log.d(TAG, "floorPlan changed to " + id);
+                Toast.makeText(WayFindingActivity.this, id, Toast.LENGTH_SHORT).show();
+                fetchFloorPlan(id);
+            }
+        }
+
+        @Override
+        public void onExitRegion(IARegion region) {
+            // leaving a previously entered region
+        }
+
+    };
+
+    private void fetchFloorPlan(String id) {
+
+        // if there is already running task, cancel it
+        cancelPendingNetworkCalls();
+
+        final IATask<IAFloorPlan> task = mResourceManager.fetchFloorPlanWithId(id);
+
+        task.setCallback(new IAResultCallback<IAFloorPlan>() {
+
+            @Override
+            public void onResult(IAResult<IAFloorPlan> result) {
+
+                if (result.isSuccess() && result.getResult() != null) {
+                    // retrieve bitmap for this floor plan metadata
+                    fetchFloorPlanBitmap(result.getResult());
+                } else {
+                    // ignore errors if this task was already canceled
+                    if (!task.isCancelled()) {
+                        // do something with error
+                        showInfo("Loading floor plan failed: " + result.getError());
+                        mOverlayFloorPlan = null;
+                    }
+                }
+            }
+        }, Looper.getMainLooper()); // deliver callbacks using main looper
+
+        // keep reference to task so that it can be canceled if needed
+        mFetchFloorPlanTask = task;
+
+    }
+
+
+    /**
+     * Load "wayfinding_graph.json" from raw resources folder of the app module
+     * @return
+     */
+    private String loadGraphJSON() {
+        try {
+            Resources res = getResources();
+            int resourceIdentifier = res.getIdentifier("wayfinding_graph", "raw", this.getPackageName());
+            InputStream in_s = res.openRawResource(resourceIdentifier);
+
+            byte[] b = new byte[in_s.available()];
+            in_s.read(b);
+            return new String(b);
+        } catch (Exception e) {
+            Log.e(TAG, "Could not find wayfinding_graph.json from raw resources folder");
+            return null;
+        }
+
+    }
+}
